1aa3a8df015cbc09197a74e896356d89d7d997a7
kamil <bitf15m531@pucit.edu.pk>
2018-06-12 22:39:58 +0500

    ali

---
+++ MvcProject/MvcProject/Controllers/LOGINController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcProject.Models;
+using System.Diagnostics;
+namespace MvcProject.Controllers
+{
+    public class LoginController : Controller
+    {
+        //
+        // GET: /LOGIN/
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public string Check(Login1 log2)
+        {
+            Login log=new Login();
+            //string name=Request["name"];
+            //string pass = Request["password"];
+            log.Logins.Add(log2);
+            return "I am CALLED.";
+        }
+	}
+}
---
+++ MvcProject/MvcProject/Controllers/Login1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcProject.Controllers
+{
+    public class Login1
+    {
+        public string username
+        {
+            set;
+            get;
+        }
+        public string password
+        {
+            set;
+            get;
+        }
+    }
+}
